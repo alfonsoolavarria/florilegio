891724aeb4f8954de3eac6f88bddc88c6459ae47
Alfonso Olavarria <alfonsojn15@gmail.com>
2026-04-09 16:22:49 -0400

    iconos nuevos

--- main_florife/management/commands/sync_bible_data.py
+++ main_florife/management/commands/sync_bible_data.py
@@ -9,18 +9,22 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--export', type=str, help='Codigo de version a exportar (ej: nbla, rv1960)')
+        parser.add_argument('--export_categories', action='store_true', help='Exporta todas las categorias y sus iconos')
         parser.add_argument('--import_file', type=str, help='Ruta del archivo JSON a importar')
 
     def handle(self, *args, **options):
         export_version = options.get('export')
+        export_categories = options.get('export_categories')
         import_file = options.get('import_file')
 
         if export_version:
             self.export_data(export_version)
+        elif export_categories:
+            self.export_categories()
         elif import_file:
             self.import_data(import_file)
         else:
-            self.stdout.write(self.style.ERROR('Debe especificar --export <version> o --import_file <archivo>'))
+            self.stdout.write(self.style.ERROR('Debe especificar --export <version>, --export_categories o --import_file <archivo>'))
 
     def export_data(self, version):
         self.stdout.write(f'Exportando version: {version}...')
@@ -45,6 +49,22 @@ class Command(BaseCommand):
         
         self.stdout.write(self.style.SUCCESS(f'Exportación completada: {filename} ({len(data["verses"])} versículos)'))
 
+    def export_categories(self):
+        from main_florife.models import Category
+        self.stdout.write('Exportando categorías e iconos...')
+        categories = Category.objects.all().values('id', 'name', 'icon')
+        
+        data = {
+            'type': 'categories',
+            'categories': list(categories)
+        }
+        
+        filename = 'categories_backup.json'
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+            
+        self.stdout.write(self.style.SUCCESS(f'Exportación de categorías completada: {filename}'))
+
     def import_data(self, file_path):
         if not os.path.exists(file_path):
             self.stdout.write(self.style.ERROR(f'Archivo no encontrado: {file_path}'))
@@ -52,6 +72,10 @@ class Command(BaseCommand):
 
         with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
+
+        if data.get('type') == 'categories':
+            self.import_categories(data)
+            return
 
         version = data.get('version')
         verses_data = data.get('verses', [])
@@ -88,3 +112,17 @@ class Command(BaseCommand):
                 )
 
         self.stdout.write(self.style.SUCCESS(f'Importación de {version} finalizada con éxito.'))
+
+    def import_categories(self, data):
+        from main_florife.models import Category
+        categories_data = data.get('categories', [])
+        self.stdout.write(f'Importando {len(categories_data)} categorías...')
+        
+        for c_data in categories_data:
+            Category.objects.update_or_create(
+                id=c_data['id'],
+                defaults={'name': c_data['name'], 'icon': c_data['icon']}
+            )
+            
+        self.stdout.write(self.style.SUCCESS('Importación de categorías finalizada.'))
+
