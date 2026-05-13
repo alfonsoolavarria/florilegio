347722dcc2a616b7abe7fed758f09a62748ae20f
EC2 Default User <ec2-user@ip-172-31-76-244.ec2.internal>
2026-05-13 17:52:19 +0000

    produccion variable

--- florilegiofe/settings.py
+++ florilegiofe/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "True" #os.getenv('DEBUG', 'False').strip() == 'True'
+DEBUG = "False" #os.getenv('DEBUG', 'False').strip() == 'True'
 
 ALLOWED_HOSTS = [os.environ.get('IP_FF'), 'localhost', '127.0.0.1']
 
