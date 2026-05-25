b4d15219ee9f11b21a771a25882c07e61147de32
Alfonso Olavarria <alfonsojn15@gmail.com>
2026-05-25 17:35:59 -0400

    fix v4.1

--- florilegiofe/settings.py
+++ florilegiofe/settings.py
@@ -194,4 +194,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Opcional: Esto ayuda a comprimir y dar caché (mejor rendimiento)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
